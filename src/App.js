a5244763e912f548574a43bbe32967fb7dbb5dec
llefevre42 <llefevre@student.42.fr>
2021-11-24 14:56:39 +0100

    image + clean appjs

--- src/App.js
+++ src/App.js
@@ -1,64 +1,10 @@
 import React from 'react';
-import { Text, View, Image, ImageBackground, Alert, Dimensions, FlatList, TextInput, StyleSheet, StatusBar, SafeAreaView, TouchableHighlight, ScrollView, TouchableOpacity, Pressable, NativeModules } from 'react-native';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
-import { NavigationContainer, getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "./screens/Home"
 import Profile from "./screens/Profile"
-
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontFamily: 'Roboto-Bold',
-    textAlign: "center"
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
-  }
-};
-
 const Stack = createNativeStackNavigator();
-
-
 
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql",
@@ -66,8 +12,6 @@ const client = new ApolloClient({
 });
 
 export default function App() {
-
-
 
   return (
     <ApolloProvider client={client}>
@@ -77,7 +21,7 @@ export default function App() {
           <Stack.Screen name="Home" component={Home} options={{
             headerShown: false,
           }} />
-           <Stack.Screen name="Profile" component={Profile} options={{
+          <Stack.Screen name="Profile" component={Profile} options={{
             headerShown: false,
           }} />
         </Stack.Navigator>
